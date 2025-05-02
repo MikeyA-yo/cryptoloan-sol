eb8f9c5ab817803f7f6f5b0b173e3c4ce5990608
MikeyA-yo <ayomideoluwatola1@gmail.com>
2025-05-02 11:14:05 +0100

    update cryptoloan

--- cryptoloan.sol
+++ cryptoloan.sol
@@ -3,5 +3,13 @@ pragma solidity >=0.4.16 <0.9.0;
 
 
 contract CryptoLoan {
-    
+    mapping(address => uint) public balances;
+
+    function store() public payable {
+      balances[msg.sender] += msg.value - (msg.value*10/100);
+    }
+
+    function getBalance() public view returns (uint) {
+    return address(this).balance;
+   }
 }
