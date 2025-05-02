239dd5e433c68994c96118662494ecb5027051f5
MikeyA-yo <ayomideoluwatola1@gmail.com>
2025-05-02 11:47:22 +0100

    update

--- cryptoloan.sol
+++ cryptoloan.sol
@@ -4,12 +4,30 @@ pragma solidity >=0.4.16 <0.9.0;
 
 contract CryptoLoan {
     mapping(address => uint) public balances;
-
+    mapping(address => uint) public highestBalances;
     function store() public payable {
-      balances[msg.sender] += msg.value - (msg.value*10/100);
+      uint fee = msg.value * 10 / 100;
+      uint depositAmount = msg.value - fee;
+      balances[msg.sender] += depositAmount;
+      if (balances[msg.sender] > highestBalances[msg.sender]) {
+        highestBalances[msg.sender] = depositAmount;   
+      }
+
     }
 
     function getBalance() public view returns (uint) {
-    return address(this).balance;
+      return address(this).balance;
+   }
+    function getHighestBalance() public view returns (uint) {
+        return highestBalances[msg.sender];
+    }
+
+   function withdraw(uint amount) public {
+    require(balances[msg.sender] >= amount, "Insufficient balance");
+
+    balances[msg.sender] -= amount;
+
+    (bool sent, ) = payable(msg.sender).call{value: amount}("");
+    require(sent, "Failed to send Ether");
    }
 }
