ac3078dd160028651736906c889d6361d7f38e7f
MikeyA-yo <ayomideoluwatola1@gmail.com>
2025-05-02 18:08:53 +0100

    i think it's fine now

--- cryptoloan.sol
+++ cryptoloan.sol
@@ -11,6 +11,11 @@ contract CryptoLoan {
     mapping(address => uint) public highestBalances;
     mapping(address => Loan) public loans;
 
+    event Deposited(address indexed user, uint amount, uint fee);
+    event Withdrawn(address indexed user, uint amount);
+    event LoanTaken(address indexed user, uint amount, uint dueDate);
+    event LoanRepaid(address indexed user, uint amount);
+
     // Deposit ETH into contract (10% fee)
     function store() public payable {
         uint fee = msg.value * 10 / 100;
@@ -20,6 +25,7 @@ contract CryptoLoan {
         if (balances[msg.sender] > highestBalances[msg.sender]) {
             highestBalances[msg.sender] = balances[msg.sender];
         }
+        emit Deposited(msg.sender, depositAmount, fee);
     }
 
     // Withdraw user funds
@@ -30,6 +36,7 @@ contract CryptoLoan {
 
         (bool sent, ) = payable(msg.sender).call{value: amount}("");
         require(sent, "Failed to send Ether");
+        emit Withdrawn(msg.sender, amount);
     }
 
     // Take a loan with a specified duration in days
@@ -42,10 +49,13 @@ contract CryptoLoan {
 
         balances[msg.sender] += _amount;
 
+        uint due = block.timestamp + periodInDays * 1 days; 
         loans[msg.sender] = Loan({
             amount: _amount,
-            dueDate: block.timestamp + (periodInDays * 1 days)
+            dueDate: due
         });
+
+        emit LoanTaken(msg.sender, _amount, due);
     }
 
     // Repay active loan
@@ -53,13 +63,16 @@ contract CryptoLoan {
         Loan storage l = loans[msg.sender];
         require(l.amount > 0, "No active loan");
         require(msg.value >= l.amount, "Insufficient repayment");
-
+ 
+        uint loanAmount = l.amount;
         // Optionally return excess ETH
         if (msg.value > l.amount) {
             payable(msg.sender).transfer(msg.value - l.amount);
         }
 
         delete loans[msg.sender]; // Loan fully repaid
+
+        emit LoanRepaid(msg.sender, loanAmount);
     }
 
     // Check if a user's loan is overdue
